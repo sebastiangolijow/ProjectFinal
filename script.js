014015a48da4c918d460f6c4624519e1a9ca74e5
sebastiangolijow <71946219+sebastiangolijow@users.noreply.github.com>
2020-11-13 11:13:47 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-const contendor= document.getElementById("test");
-const botonRES= document.getElementById("boton");
-const resultadoTEST= document.getElementById("resultado");
+const contendor = document.getElementById("test");
+const botonRES = document.getElementById("boton");
+const resultadoTEST = document.getElementById("resultado");
 
-const preguntas= [
+const preguntas = [
     {
         pregunta: "1. Tuviste contacto con pacientes Covid positivo?",
         respuestas: {
 
             A: "si", B: "no"
         }, respuestaCorrecta: "A"
-    }, 
+    },
     {
         pregunta: "1. Tuviste fiebre en los ultimos dias?",
         respuestas: {
@@ -42,9 +42,9 @@ const preguntas= [
 function mostrarTest() {
     const preguntasYrespuestas = [];
 
-    preguntas.forEach((preguntaActual, numeroDePregunta)=> {
+    preguntas.forEach((preguntaActual, numeroDePregunta) => {
 
-    const respuestas=[];
+    const respuestas = [];
         for (letraRespuesta in preguntaActual.respuestas){
             respuestas.push(
         `
@@ -52,29 +52,34 @@ function mostrarTest() {
         <input type="radio" name="${numeroDePregunta}" value="${letraRespuesta}">
         ${letraRespuesta} : ${preguntaActual.respuestas[letraRespuesta]}
         </label>`
-            );
+        );
         } preguntasYrespuestas.push(
         `<div class="cuestion"> ${preguntaActual.pregunta} </div>
         <div class="respuestas"> ${respuestas.join('')} </div>`   
         );
     });
-    contendor.innerHTML= preguntasYrespuestas.join('');
+
+    contendor.innerHTML = preguntasYrespuestas.join('');
 } mostrarTest();
 
 function mostrarResultado(){
-    const respuestas = contenedor.querySelectorAll(".respuestas");
+    const respuestas = contendor.querySelectorAll(".respuestas");
     let respuestasCorrectas = 0;
-    preguntas.forEach((preguntaActual, numeroDePregunta) => 
-    {const todasLasRespuestas= respuestas[numeroDePregunta]; 
+
+    preguntas.forEach((preguntaActual, numeroDePregunta) => {
+    const todasLasRespuestas = respuestas[numeroDePregunta]; 
     const checkboxRespuestas = `input[name='${numeroDePregunta}']:checked`;
     const respuestaElegida = (todasLasRespuestas.querySelector(checkboxRespuestas)|| {}).value;
-      if (respuestaElegida === preguntaActual.respuestaCorrecta) {
-          respuestasCorrectas++;  respuestas[numeroDePregunta].style.color='blue';
+      
+    if (respuestaElegida === preguntaActual.respuestaCorrecta){
+          respuestasCorrectas++; 
+        
+          respuestas[numeroDePregunta].style.color='blue';
       }
       else {
           respuestas[numeroDePregunta].style.color='red';
       }
     });
-    resultadoTEST.innerHTML='Usted ha acertado ' + respuestasCorrectas + 'preguntas de un total de ' + preguntas.length; 
+    resultadoTEST.innerHTML='Usted ha acertado ' + respuestasCorrectas + ' preguntas de un total de ' + preguntas.length; 
 }
 botonRES.addEventListener('click', mostrarResultado); 
